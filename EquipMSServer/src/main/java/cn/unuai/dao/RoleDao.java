d2951ff9c6e605f1a85baa65a04d02f2e4de431a
Your Name <920573625@qq.com>
2018-06-08 01:00:56 +0800

    2018-6-8备份

--- EquipMSServer/src/main/java/cn/unuai/dao/RoleDao.java
+++ EquipMSServer/src/main/java/cn/unuai/dao/RoleDao.java
@@ -2,6 +2,7 @@ package cn.unuai.dao;
 
 import cn.unuai.entity.DepartmentEntity;
 import cn.unuai.entity.RoleEntity;
+import cn.unuai.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,4 +17,7 @@ public interface RoleDao extends JpaRepository<RoleEntity, Long> {
 
     @Query("select rolename from RoleEntity where id=?1")
     public String getRoleName(int id);
+
+    @Query("select e from RoleEntity e where e.id=?1")
+    public RoleEntity findById(int id);
 }
